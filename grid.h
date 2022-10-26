1399a170f6fae188856f81da6710a7dc289d2c9d
AlekTunik <tunik.a@northeastern.edu>
2022-10-26 12:55:36 -0400

    added grid header file

---
+++ grid.h
@@ -0,0 +1,17 @@
+// Project #3: Word Search Part A
+// 
+// Group Members: Lisa Byrne, Kaite O'Flaherty, Alek Tunik
+//
+// Description: Header file for the grid class
+// Assumption: 
+
+#include <iostream>
+#include <stdlib.h>
+
+class grid
+{
+    public:
+
+    private: 
+    
+};
