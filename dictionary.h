067a66247a80736dd1b2cf52ccf5bbe6468cbcf4
AlekTunik <tunik.a@northeastern.edu>
2022-10-26 12:46:13 -0400

    added dict header file

---
+++ dictionary.h
@@ -0,0 +1,17 @@
+// Project #3: Word Search Part A
+// 
+// Group Members: Lisa Byrne, Kaite O'Flaherty, Alek Tunik
+//
+// Description: Header file for the dictionary class
+// Assumption: 
+
+#include <iostream>
+#include <stdlib.h>
+
+class dictionary
+{
+    public:
+
+    private: 
+    
+};
