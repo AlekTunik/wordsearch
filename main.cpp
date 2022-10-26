559183167f1a43ac7f0e725b207a178f24acc0f0
AlekTunik <tunik.a@northeastern.edu>
2022-10-26 17:02:26 -0400

    main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Project #3: Word Search Part A
+// 
+// Group Members: Lisa Byrne, Kaite O'Flaherty, Alek Tunik
+//
+// Description: Main file for word search project
+// Assumption: 
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector> 
+
+using namespace std;
+
