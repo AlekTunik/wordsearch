973c2d3717614936210c0a87dca087bd03bdd3c4
Kaitlyn O’Flaherty <kaitlynoflaherty@Kaitlyns-MacBook-Pro.local>
2022-10-28 11:07:08 -0400

    starting main/ global funcs

--- main.cpp
+++ main.cpp
@@ -9,12 +9,33 @@
 #include <stdlib.h>
 #include <string>
 #include <vector> 
+#include "dictionary.h"
+#include "grid.h"
+#include "d_matrix.h"
+#include "d_except.h"
 
 using namespace std;
 
-int findMatches()
+int findMatches(dictionary dict, grid myGrid)
 // Function to print out candidate words that can be found in the dictionary.
 {
+    // Initialize variables
+    string word;
+    vector <string> found;
+
+    //scan grid to find all possible words
+    for (int i = 0; i < myGrid.getRows(); i ++)
+    // iterate thru rows
+    {
+        for (int j = 0; j < myGrid.getCols(); j ++)
+        // iterate thru cols
+        {
+
+        }
+    }
+
+    //print out candidate words
+
 
 } // end findMatches
 
